87dcf09c2d11ca5b13c52507e19a45bb34bb9560
Paris Liu <paris0120@gmail.com>
2020-01-04 08:05:53 -0500

    Initial commit

---
+++ src/main/java/com/parisliu/WRDS_Sync/App.java
@@ -0,0 +1,13 @@
+package com.parisliu.WRDS_Sync;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
